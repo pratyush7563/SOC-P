506b81b963d0b66bb53909b7f91645f63faeec03
pratyush7563 <34338031+pratyush7563@users.noreply.github.com>
2018-06-21 19:18:33 +0700

    Add files via upload

---
+++ progress.py
@@ -0,0 +1,72 @@
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.model_selection import train_test_split # function for splitting data to train and test sets
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.classify import SklearnClassifier
+
+from wordcloud import WordCloud,STOPWORDS
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+from subprocess import check_output
+
+data = pd.read_csv('../input/Sentiment.csv')
+# Keeping only the neccessary columns
+data = data[['text','sentiment']]
+# Splitting the dataset into train and test set
+train, test = train_test_split(data,test_size = 0.1)
+# Removing neutral sentiments
+train = train[train.sentiment != "Neutral"]
+train_pos = train[ train['sentiment'] == 'Positive']
+train_pos = train_pos['text']
+train_neg = train[ train['sentiment'] == 'Negative']
+train_neg = train_neg['text']
+
+def wordcloud_draw(data, color = 'black'):
+    words = ' '.join(data)
+    cleaned_word = " ".join([word for word in words.split()
+                            if 'http' not in word
+                                and not word.startswith('@')
+                                and not word.startswith('#')
+                                and word != 'RT'
+                            ])
+    wordcloud = WordCloud(stopwords=STOPWORDS,
+                      background_color=color,
+                      width=2500,
+                      height=2000
+                     ).generate(cleaned_word)
+    plt.figure(1,figsize=(13, 13))
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.show()
+    
+print("Positive words")
+wordcloud_draw(train_pos,'white')
+print("Negative words")
+wordcloud_draw(train_neg)
+tweets = []
+stopwords_set = set(stopwords.words("english"))
+
+for index, row in train.iterrows():
+    words_filtered = [e.lower() for e in row.text.split() if len(e) >= 3]
+    words_cleaned = [word for word in words_filtered
+        if 'http' not in word
+        and not word.startswith('@')
+        and not word.startswith('#')
+        and word != 'RT']
+    words_without_stopwords = [word for word in words_cleaned if not word in stopwords_set]
+    tweets.append((words_without_stopwords, row.sentiment))
+
+test_pos = test[ test['sentiment'] == 'Positive']
+test_pos = test_pos['text']
+test_neg = test[ test['sentiment'] == 'Negative']
+test_neg = test_neg['text']

0f02d030639e35785e76dc96f2552a359c52e548
bhavita2404 <35843719+bhavita2404@users.noreply.github.com>
2018-06-21 21:30:33 +0530

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -5,7 +5,19 @@
 <script src='https://www.google.com/recaptcha/api.js'></script>
 <title>HomePage</title>
 <style>
-
+.butt
+{
+	background-color: #4CAF50;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+}
 .button{
   background-color :#9933FF;
   border : none; 
@@ -16,7 +28,7 @@
   font-size:25px;
   }
   .but{
-     background-color :#32CD32;
+     background-color :#FF0000;
 	 color :white;
 	 padding: 7px;
 	 text-align:center;
@@ -114,6 +126,44 @@
     }
 }
 
+
+/* The message box is shown when the user clicks on the password field */
+#message {
+    display:none;
+    background: #f1f1f1;
+    color: #000;
+    position: relative;
+    padding: 20px;
+    margin-top: 10px;
+}
+
+#message p {
+    padding: 10px 35px;
+    font-size: 18px;
+}
+
+/* Add a green text color and a checkmark when the requirements are right */
+.valid {
+    color: green;
+}
+
+.valid:before {
+    position: relative;
+    left: -35px;
+    content: "✔";
+}
+
+/* Add a red text color and an "x" when the requirements are wrong */
+.invalid {
+    color: red;
+}
+
+.invalid:before {
+    position: relative;
+    left: -35px;
+    content: "✖";
+}
+
 </style>
 </head>
 <body>
@@ -131,19 +181,35 @@
 
 <div class="footer">
 <p align="center">
-<button type="button" class="button" onclick="document.getElementById('wrapper').style.display='block'" value="Check Your Status">Check Your Status</button>
+<button type="button" class="button" onclick="document.getElementById('sign').style.display='block'" value="Check Your Status">Check Your Status</button>
 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 
 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp 
 <button type="button" class="button" onclick="location.href='lifestyle.html'" value="Healthy LifeStyle">Healthy LifeStyle</button>
 </p>
 </div>
 
+<div class="modal" id="sign">
+<div class="modal-content animate" align="center">
+<div class="container">
+<button value="SignIn" class="butt" onclick="document.getElementById('wrapper').style.display='block'">SignIn</button> &nbsp &nbsp &nbsp &nbsp <b style="text-size:15px;">OR</b>
+&nbsp &nbsp &nbsp &nbsp <button value="SignUp" class="butt" onclick="document.getElementById('gotto').style.display='block'">SignUp</button><br><br>
+
+
+      <button type="button" class="but" align="center" onclick="document.getElementById('sign').style.display='none'" class="cancelbtn">Cancel</button><br><br>
+
+    
+</div>
+</div>
+</div>
+
 <div class="modal" id="wrapper">
 
 <form class="modal-content animate" action="/action_page.php" align="center" method="post">
 <div class="container">
+<label for="n1">
 <b style="font-size:20px;">UserName-</b><br>
-<input type="text" name="n1" class="input" placeholder="Enter your username"><br><br>
+</label>
+<input type="text" id="n1" name="n1" class="input" placeholder="Enter your username" required><br><br>
 <!--
 <select >
 <option value=""disabled selected>Select your option</option>
@@ -151,13 +217,36 @@
 <option value="">victim</option>
 </select><br><br>
 -->
-<b style="font-size:20px;">Password-</b><br>
-<input type="password" placeholder="Enter Password" class="input" name="password"><br><br>
+<label for="password" >
+<b style="font-size:20px;">Password-</b><br></label>
+
+
+<input type="password"  placeholder="Enter Password" class="input" name="password" id="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required><br>
+
+<input type="checkbox" onclick="myFunction()">Show Password<br><br>
+<script>
+function myFunction()
+{
+	var x=document.getElementById("password");
+	
+		if(x.type===password)
+		{
+			x.type=text;
+		}
+		else
+		{
+			x.type=password;
+		}
+	
+}
+</script>
+
  <div align="center" class="g-recaptcha" data-sitekey="6LfTBF8UAAAAALzMeAHb1Zp_Styckvt9HCGgQ8qx" required></div><br><br>
 
 
 
-<button class="but" type="submit" value="submit" align="right" onclick="page3.html">Submit</button><br><br>
+<button class="but" style="background-color:#008000;"type="submit" value="submit" align="right" onclick="page3.html">Submit</button><br><br>
 </div>
 
 <label>
@@ -168,6 +257,99 @@
 	  
 <div class="container" >
       <button type="button" class="but" onclick="document.getElementById('wrapper').style.display='none'" class="cancelbtn">Cancel</button><br><br>
+
+    </div>
+
+	</form>
+	
+	<?php
+if(isset($_POST['submit']))
+{
+function CheckCaptcha($userResponse) {
+        $fields_string = '';
+        $fields = array(
+            'secret' => '6LfTBF8UAAAAAHdSQ5a7hlA9nICL05TZ5Mydazgd',
+            'response' => $userResponse
+        );
+        foreach($fields as $key=>$value)
+        $fields_string .= $key . '=' . $value . '&';
+        $fields_string = rtrim($fields_string, '&');
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+        $res = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($res, true);
+    }
+    // Call the function CheckCaptcha
+    $result = CheckCaptcha($_POST['g-recaptcha-response']);
+    if ($result['success']) {
+        //If the user has checked the Captcha box
+        echo "Captcha verified Successfully";
+    } else {
+        // If the CAPTCHA box wasn't checked
+       echo '<script>alert("Error Message");</script>';
+    }
+}
+    ?>
+	
+	</div>
+	
+	<div class="modal" id="gotto">
+
+<form class="modal-content animate" action="/action_page1.php" align="center" method="post">
+<div class="container">
+<b style="font-size:20px;">UserName-</b><br>
+<input type="text" name="n1" class="input" placeholder="Enter your username" required><br><br>
+<!--
+<select >
+<option value=""disabled selected>Select your option</option>
+<option  value="Parent or Guide">Parent or Guide</option>
+<option value="">victim</option>
+</select><br><br>
+-->
+<label for="pas" >
+<b style="font-size:20px;">Password-</b><br></label>
+
+
+<input type="password"  placeholder="Enter Password" class="input" name="pas" id="pas" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required><br>
+ 
+ <input type="checkbox" onclick="myFunction()">Show Password<br><br>
+<script>
+function myFunction()
+{
+	var x=document.getElementById("pas");
+	
+		if(x.type===password)
+		{
+			x.type=text;
+		}
+		else
+		{
+			x.type=password;
+		}
+	
+}
+ </script>
+ 
+ <div align="center" class="g-recaptcha" data-sitekey="6LfTBF8UAAAAALzMeAHb1Zp_Styckvt9HCGgQ8qx" required></div><br><br>
+
+
+
+<button class="but" style="background-color:#008000;" type="submit" value="submit" align="right" onclick="page3.html">Submit</button><br><br>
+</div>
+
+<!--<label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me &nbsp  &nbsp &nbsp &nbsp &nbsp &nbsp 
+		&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 
+		<span class="psw">Forgot <a href="#">password?</a></span><br><br>
+      </label>-->
+	  
+<div class="container" >
+      <button type="button" class="but" onclick="document.getElementById('gotto').style.display='none'" class="cancelbtn">Cancel</button><br><br>
 
     </div>
 
@@ -219,6 +401,25 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+var modal = document.getElementById('gotto');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+var modal = document.getElementById('sign');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 </script>
 </body>
 </html>
